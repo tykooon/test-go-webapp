366ed8a768297f68b5b0d420b1f99ed6c6975fd7
AlexTykoon <alexandertykun@coherentsolutions.com>
2023-04-05 16:34:57 +0300

    Start up try 1.1

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,12 +10,17 @@ import (
 
 func main() {
 
+	logger := log.New(os.Stdout, "APP LOG :", log.Lshortfile)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		logger.Fatal("Error: port must be set")
+	}
+
 	u1 := usersys.User{
 		FirstName: "Alex",
 		LastName:  "Tykoun",
 	}
-
-	logger := log.New(os.Stdout, "APP LOG :", log.Lshortfile)
 
 	app := app{
 		user:      u1,
@@ -23,6 +28,6 @@ func main() {
 		templates: initTemplates(),
 	}
 
-	err := http.ListenAndServe(":5000", app.routes())
+	err := http.ListenAndServe(":"+port, app.routes())
 	app.log.Fatal(err)
 }
